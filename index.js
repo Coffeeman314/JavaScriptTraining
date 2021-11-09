0e8e2ebc9cc422e056b0c753f305073dc7d1f92f
Igor <i.pavlyshyn05@gmail.com>
2021-11-09 22:07:33 +0100

    trello refactoring

--- index.js
+++ index.js
@@ -1,60 +1,45 @@
-// primeData
 const burrito = 15.678;
 const steak = 123.965;
 const ramen = 90.2345;
 
-// exercise 1
 const maxPrice = Math.max(burrito, steak, ramen);
 console.log(Math.max(burrito, steak, ramen));
 
-// document.writeIn(<p>'some text'</p>)
-// exercise 2
 const minPrice = Math.min(burrito, steak, ramen);
 console.log(minPrice);
 
-// exercise 3
 const sumPrices = burrito + ramen + steak;
 console.log(sumPrices);
 
-// exercise 4
 const floorBurrito = Math.floor(burrito);
 const floorSteak = Math.floor(steak);
 const floorRamen = Math.floor(ramen);
 const sumFloorFood = floorBurrito + floorSteak + floorRamen;
 console.log(sumFloorFood);
 
-// exercise 6
 const preRound = sumFloorFood / 100;
 const roundToHundred = Math.round(preRound) * 100;
 console.log(roundToHundred);
 
-// const preRound = sumFloorFood/100
-// const roundToHundred = Math.ceil (preRound)*100
-// console.log (roundToHundred)
-
-// exercise 7
-
 function testNum(sumFloorFood) {
-  let result;
-  if (sumFloorFood % 2 == 0) {
-    result = "Yep, it's even.";
+  
+  if (sumFloorFood % 2 === 0) {
+    return "Yep, it's even";
   } else {
-    result = "Emm, nope...";
+    return "Emm, nope...";
   }
-  return result;
+  
 }
 console.log(testNum(sumFloorFood));
 
-// exercise 8
 const takeAll = burrito + steak + ramen;
-const keepTheChangeBitch = 500 - takeAll;
+const money = 500;
+const keepTheChangeBitch = money - takeAll;
 console.log(keepTheChangeBitch);
 
-// exercise 9
 const averageValue = takeAll / 3;
 console.log(averageValue.toFixed(2));
 
-// exercise 10
 const discount = Math.round(Math.random() * 100);
 
 const cost = takeAll / 2;
@@ -65,46 +50,18 @@ console.log(needToPay.toFixed(2));
 const profit = cost - discountValue;
 console.log(profit.toFixed(2));
 
-// last part
-document.writeln("Maximal price:");
-document.writeln(maxPrice);
-document.writeln("<br/>");
-
-document.writeln("Minimal price:");
-document.writeln(minPrice);
-document.writeln("<br/>");
-
-document.writeln("Summary price:");
-document.writeln(sumPrices);
-document.writeln("<br/>");
-
-document.writeln("Summary price without coins:");
-document.writeln(sumFloorFood);
-document.writeln("<br/>");
-
-document.writeln("Rounded to hundred:");
-document.writeln(roundToHundred);
-document.writeln("<br/>");
-
-document.writeln("Is that value even?");
-document.writeln(testNum(sumFloorFood));
-document.writeln("<br/>");
-
-document.writeln("I have 500$. Okay, your change is");
-document.writeln(keepTheChangeBitch);
-document.writeln("<br/>");
-
-document.writeln("Average value:");
-document.writeln(averageValue.toFixed(2));
-document.writeln("<hr/>");
-
-document.writeln("Let's imagine prikolejshn. If you'll have discount");
-document.writeln(discount);
-
-document.writeln("%, you pay me");
-document.writeln(needToPay.toFixed(2));
-document.writeln("$,")
-document.writeln("<br/>");
-document.writeln("than my profit will be only");
-document.writeln(profit.toFixed(2));
-document.writeln("$. Isn't it creepy?");
+let container=document.querySelector("#container");
+container.innerHTML=`
+<p>Maximal price: ${maxPrice};</p>
+<p>Minimal price: ${minPrice};</p>
+<p>Summary price: ${sumPrices};</p>
+<p>Each item without coins: ${sumFloorFood};</p>
+<p>Rounded to hundred: ${roundToHundred};</p>
+<p>Is that value even? ${testNum(sumFloorFood)};</p>
+<p>I have 500$. Okay, your change is ${keepTheChangeBitch};</p>
+<p>Average value: ${averageValue.toFixed(2)};</p>
+<p>Let's imagine prikolejshn.</p>
+<p>If you'll have discount ${discount}%,</p>
+<p>You pay me ${needToPay.toFixed(2)}$,<p>
+<p>Than my profit will be only ${profit.toFixed(2)}$. Isn't it creepy?<p>
+`;
