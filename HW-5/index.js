8cc258d45c8d1d50c52f8e5b80c38f463fbdb6e0
Igor <i.pavlyshyn05@gmail.com>
2021-12-16 17:39:06 +0100

    homework-5 commit

---
+++ HW-5/index.js
@@ -0,0 +1,61 @@
+
+// 1. Get array of random numbers
+const getRandomArray = (length, min, max) => {
+  let randomArr = [];
+  for (let i = 0; i < length; i++) {
+    const getRandom = (min, max) =>
+      Math.floor(Math.random() * (max - min + 1)) + min;
+    randomArr.push(getRandom(min, max));
+  }
+  return randomArr;
+};
+console.log('1. Random array: ', getRandomArray(10, 1, 99));
+
+// 2. getAverage function
+const numbers = [2, 5, 4, 3, 6, 7, 4];
+const getAverage = (numbers) => {
+  const summary = numbers.reduce((summary, number) => {
+    return summary + number;
+  }, 0);
+  return summary / numbers.length;
+};
+console.log('2. Average count: ', getAverage(numbers).toFixed(2));
+
+// 3.
+const allNumbers = [1, 2, 110, 4, 15, 8, 55, 99];
+const oddOnly = allNumbers
+  .filter((number) => Math.floor(number) === number)
+  .filter((number) => number % 2 != 0);
+console.log('3. Filtered array: ', oddOnly.join(", "));
+
+// 4.
+const taskFiveArr = [-1, 2, 5, -9, -4, 1, 14, -7];
+const positiveNumbers = taskFiveArr.filter((number) => number > 0);
+console.log('4. Positive numbers count: ', positiveNumbers.length);
+
+// 5.
+const getDividedByFive = allNumbers.filter((number) => number % 5 === 0);
+console.log('5. Divided by 5 numbers: ', getDividedByFive);
+
+// 6. generateCombinations
+function permutator(inputArr) {
+  var results = [];
+
+  function permute(arr, memo) {
+    var cur,
+      memo = memo || [];
+    for (var i = 0; i < arr.length; i++) {
+      cur = arr.splice(i, 1);
+      if (arr.length === 0) {
+        results.push(memo.concat(cur));
+      }
+      permute(arr.slice(), memo.concat(cur));
+      arr.splice(i, 0, cur[0]);
+    }
+    
+    return results;
+  }
+
+  return permute(inputArr);
+}
+console.log('6. Al combinations: ', permutator("abc".split("")).join("//"));
