840bac8f130076275386bf1bb57e9aab9e3fab6c
Igor <i.pavlyshyn05@gmail.com>
2022-04-13 20:36:16 +0300

    hw-10 commmit

---
+++ HW-10/index.js
@@ -0,0 +1,24 @@
+const buttonNodeList = document.querySelectorAll(".btn");
+const btncont = document.querySelector(".buttonContainer");
+
+const soundArr = ["boop", "deep_whoosh", "fly_by", "thump", "whoosh"];
+const letterArray = ["A", "S", "D", "F", "G"];
+const soundArray = soundArr.map((item, index) => {
+  const sound = new Audio(`./sounds/sounds_${item}.mp3`);
+  sound.setAttribute("id", `btn-${index + 1}`);
+  sound.setAttribute("code", "Key" + letterArray[index]);
+  return sound;
+});
+console.log(soundArray[0].attributes.code.value);
+
+document.addEventListener("keydown", (event) => {
+  console.trace();
+  soundArray.forEach(
+    (item) => event.code === item.attributes.code.value && item.play()
+  );
+});
+
+btncont.addEventListener("click", (event) => {
+  console.trace();
+  soundArray.forEach((item) => event.target.id === item.id && item.play());
+});
