4d2c27c16725c4289567ac50250174dfa5a42d4e
Igor <i.pavlyshyn05@gmail.com>
2022-06-20 01:04:10 +0300

    hw13 commit

---
+++ HW-13/index.js
@@ -0,0 +1,28 @@
+const convertString = () =>
+  String.fromCharCode(Date.now().toString().slice(-5));
+
+const getRandomChinese = (n) => {
+  return new Promise((resolve, reject) => {
+    if (n > 0) {
+      let str = "";
+      for (let i = 0; i < n; i++) {
+        setTimeout(() => {
+          str += convertString();
+          if (i + 1 === n) {
+            resolve(str);
+          }
+        }, (i + 1) * 50);
+      }
+    } else {
+      reject();
+    }
+  });
+};
+
+getRandomChinese(1)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((reject) => {
+    console.log("wrong num");
+  });
