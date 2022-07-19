30e06c793b8c199a07811d76a337981c94209780
Igor <i.pavlyshyn05@gmail.com>
2022-07-19 19:09:47 +0300

    hw14 commit

---
+++ HW-14/src/index.js
@@ -0,0 +1,7 @@
+
+import '../../HW-4/src/index.js';
+import '../../HW-5/src/index.js';
+import '../../HW-6/index.js';
+import '../../HW-7/index.js';
+import '../../HW-8/index.js';
+import '../../HW-9/index.js';
